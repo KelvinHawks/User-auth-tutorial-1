5d714b3af255ade37423731a1273f9d0f9d7939f
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-07-30 15:26:41 +0300

    Add .env to gitignore

--- server/app.js
+++ server/app.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const connectDB = require('./connectDB')
-const {router} = require('./router/router')
-const cors = require('cors')
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./connectDB");
+const { router } = require("./router/router");
+const cors = require("cors");
 
-dotenv.config()
-const app = express()
-app.use(cors())
-app.use(express.json())
-const PORT = 5000
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = 8000;
 
-connectDB()
+connectDB();
 
-app.use('/api/users', router)
+app.use("/api/users", router);
 
-app.listen(PORT, ()=>{
-    console.log(`app listening on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
